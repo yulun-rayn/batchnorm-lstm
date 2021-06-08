0c67c653e8be8d8703d6b7989dfe0f3317842541
Yulun "Rayn" Wu <40144394+yulun-rayn@users.noreply.github.com>
2021-06-08 14:42:55 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="batchnorm-lstm",
     version="0.0.1",
-    author="Zyx Masic",
-    author_email="zyx.masic@gmail.com",
+    author="Yulun Rayn Wu",
+    author_email="yulun_wu@berkeley.edu",
     description="A package that implements Many-to-One Long Short-Term Memory with batch normalization, dropout and layer stacking.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Zyx0Wu/batchnorm-lstm",
+    url="https://github.com/yulun-rayn/batchnorm-lstm",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
